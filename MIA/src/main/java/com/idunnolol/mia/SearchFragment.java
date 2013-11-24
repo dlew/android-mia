6200b1c07120eb94c5341fbf296931d25eec778d
Daniel Lew <dan@mobiata.com>
2013-11-24 09:43:41 -0600

    Added basic outline for # input
    Also got rid of some unnecessary things from app

---
+++ MIA/src/main/java/com/idunnolol/mia/SearchFragment.java
@@ -0,0 +1,18 @@
+package com.idunnolol.mia;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
