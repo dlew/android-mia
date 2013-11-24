0bfc9b4d743fad126d47b15b662362639ab4db6e
Daniel Lew <dan@mobiata.com>
2013-11-24 09:48:07 -0600

    Added bit of query logic

--- MIA/src/main/java/com/idunnolol/mia/SearchFragment.java
+++ MIA/src/main/java/com/idunnolol/mia/SearchFragment.java
@@ -2,17 +2,42 @@ package com.idunnolol.mia;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchFragment extends Fragment {
+
+    private EditText mEditText;
+    private Button mButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mEditText = (EditText) rootView.findViewById(R.id.search_edit_text);
+        mButton = (Button) rootView.findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search(mEditText.getText().toString());
+            }
+        });
+
         return rootView;
     }
 
+    private void search(String text) {
+        if (TextUtils.isEmpty(text)) {
+            Toast.makeText(getActivity(), getString(R.string.error_no_query), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // TODO: Query for stop #
+        }
+    }
 }
